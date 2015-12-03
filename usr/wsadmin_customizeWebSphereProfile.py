5d83202b4b8d8a598e60bc25e74bcf8cbbb4f6df
tarabi <tarabi@redhat.com>
2015-12-03 15:53:09 -0500

    added post install config of console port. can access no redirect. however stepping on other gears consoles....to be investigated further

--- usr/wsadmin_customizeWebSphereProfile.py
+++ usr/wsadmin_customizeWebSphereProfile.py
@@ -1,62 +1,17 @@
+#READ CLI ARGUMENTS
 ###############################################################################
-# READ CLI ARGUMENTS
+OPENSHIFT_WEBSPHERE_IP = sys.argv[0]
+OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT = sys.argv[1]
+
+print "echoing argvs"
+
+print OPENSHIFT_WEBSPHERE_IP
+print OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT
 ###############################################################################
-OPENSHIFT_GEAR_DNS = sys.argv[0]
-OPENSHIFT_WEBSPHERE_IP = sys.argv[1]
-
+#Setup the Console public port to be the Public port assigned by OSE not 9043
 ###############################################################################
-# Virtual Hosts: make sure connections from every hostname are accepted
-###############################################################################
-vhosts = AdminConfig.list('VirtualHost').split(java.lang.System.getProperty("line.separator"))
-for vhost in vhosts:
-	endpointString = AdminConfig.showAttribute(vhost, "aliases")
-	endpointList = endpointString[1:len(endpointString)-1].split(" ")
-	for endpoint in endpointList:
-		AdminConfig.modify(endpoint, '[[hostname "' + '*' + '"]]')
 
-###############################################################################
-# Configure WebSphere to use a specific IP address
-# http://www-01.ibm.com/support/knowledgecenter/SSAW57_8.5.5/com.ibm.websphere.nd.doc/ae/trun_multiplenic.html?lang=en
-###############################################################################
-# Customize ORB service
-orb = AdminConfig.list('ObjectRequestBroker')
-orbPropertiesString = AdminConfig.showAttribute(orb, "properties")
-orbPropertiesList = orbPropertiesString[1:len(orbPropertiesString)-1].split(" ")
-# Remove specific existing properties
-for orbProperty in orbPropertiesList:
-	name = AdminConfig.showAttribute(orbProperty, "name")
-	if name == "com.ibm.CORBA.LocalHost":
-		AdminConfig.remove(orbProperty)
-	elif name == "com.ibm.ws.orb.transport.useMultiHome":
-		AdminConfig.remove(orbProperty)
-
-# Add new properties
-attr = []
-attr.append([['name','com.ibm.CORBA.LocalHost'],['required','true'],['value', OPENSHIFT_WEBSPHERE_IP]])
-AdminConfig.modify(orb, [['properties', attr]])
-attr = []
-attr.append([['name','com.ibm.ws.orb.transport.useMultiHome'],['required','false'],['value','false']])
-AdminConfig.modify(orb, [['properties', attr]])
-
-# Customize JVM custom properties
-jvm = AdminConfig.list('JavaVirtualMachine')
-jvmPropertiesString = AdminConfig.showAttribute(jvm, "systemProperties")
-jvmPropertiesList = jvmPropertiesString[1:len(jvmPropertiesString)-1].split(" ")
-# Remove specific existing properties
-for jvmProperty in jvmPropertiesList:
-	name = AdminConfig.showAttribute(jvmProperty, "name")
-	if name == "com.ibm.websphere.network.useMultiHome":
-		AdminConfig.remove(jvmProperty)
-
-# Add new properties
-attr = []
-attr.append([['name','com.ibm.websphere.network.useMultiHome'],['required','false'],['value', 'false']])
-AdminConfig.modify(jvm, [['systemProperties', attr]])
-
-# Bind all ports to GEAR IP
-endpoints = AdminConfig.list('EndPoint').split(java.lang.System.getProperty("line.separator"))
-for endpoint in endpoints:
-	AdminConfig.modify(endpoint, '[[host ' + OPENSHIFT_WEBSPHERE_IP + ']]')
+AdminTask.modifyServerPort('server1', '[-nodeName OpenShiftNode01 -endPointName WC_adminhost_secure -host ' +  OPENSHIFT_WEBSPHERE_IP + ' -port ' + OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT + ']')
 
 ###############################################################################
 AdminConfig.save()
