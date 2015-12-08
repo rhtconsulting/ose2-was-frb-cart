a1798b011ba537cc8ccda4ad419b1d94da034709
tarabi <tarabi@redhat.com>
2015-12-08 16:46:21 -0500

    updating killing of processes of WAS and console port config disabled

--- usr/wsadmin_customizeWebSphereProfile.py
+++ usr/wsadmin_customizeWebSphereProfile.py
@@ -3,7 +3,6 @@
 OPENSHIFT_WEBSPHERE_IP = sys.argv[0]
 OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT = sys.argv[1]
 
-print "echoing argvs"
 
 print OPENSHIFT_WEBSPHERE_IP
 print OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT
@@ -11,24 +10,25 @@ print OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT
 #Setup the Console public port to be the Public port assigned by OSE not 9043
 ###############################################################################
 
-AdminTask.modifyServerPort('server1', '[-nodeName OpenShiftNode01 -endPointName WC_adminhost_secure -host ' +  OPENSHIFT_WEBSPHERE_IP + ' -port ' + OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT + ']')
+#AdminTask.modifyServerPort('server1', '[-nodeName OpenShiftNode01 -endPointName WC_adminhost_secure -host ' +  OPENSHIFT_WEBSPHERE_IP + ' -port ' + OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT + ']')
 
-vhosts = AdminConfig.list('VirtualHost').split(java.lang.System.getProperty("line.separator"))
-for vhost in vhosts:
-        endpointString = AdminConfig.showAttribute(vhost, "aliases")
-        endpointList = endpointString[1:len(endpointString)-1].split(" ")
-        #print endpointString
-        for endpoint in endpointList:
+#vhosts = AdminConfig.list('VirtualHost').split(java.lang.System.getProperty("line.separator"))
+#for vhost in vhosts:
+#        endpointString = AdminConfig.showAttribute(vhost, "aliases")
+#        endpointList = endpointString[1:len(endpointString)-1].split(" ")
+#        #print endpointString
+#        for endpoint in endpointList:
                 #print "%s" %(AdminConfig.showAttribute(endpoint, 'port'));
                 #print endpoint
 
-                pound_index = endpoint.index('#')
-                split_endpoint = endpoint[pound_index+1:len(endpoint)-1]
+#                pound_index = endpoint.index('#')
+#                split_endpoint = endpoint[pound_index+1:len(endpoint)-1]
                 #print endpoint
-                if "HostAlias_5"  == split_endpoint:
-                  print "%s" %(AdminConfig.showAttribute(endpoint, 'port'));
+#                if "HostAlias_5"  == split_endpoint:
+#                  print "%s" %(AdminConfig.showAttribute(endpoint, 'port'));
                   #AdminConfig.modify("HostAlias", vhost, [["hostname", "HostAlias_5" ], ["port", OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT]])
-                  AdminConfig.modify(endpoint, [['port', OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT]])
+#                  AdminConfig.modify(endpoint, [['port', OPENSHIFT_WEBSPHERE_WC_ADMINHOST_SECURE_PROXY_PORT]])
+
 ###############################################################################
 
 ###############################################################################
